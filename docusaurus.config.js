f90f9c056f1a30bb25cc9175465a4e4df1bd23e0
port <108868128+portdeveloper@users.noreply.github.com>
2025-03-03 05:12:42 -0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: "https://scaffold-eth-2-docs.vercel.app",
+  url: "https://docs.scaffoldeth.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
